4800c5cb7ecab551706f9e483799d4514b1839ab
GijsvdP21 <gijsvandeputte@icloud.com>
2023-04-19 11:33:08 +0100

    push

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+// Maak een nieuwe express app
+const app = express()
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+// Maak een route voor de index
+app.get('/', function (req, res) {
+    // res.send('Hello World!')
+    res.render('index')
+  })
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
